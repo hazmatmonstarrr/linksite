637419b0ec17a139b88a42585d16f6ba935bd2a6
Mayumi <mayumishinozuka@gmail.com>
2025-07-26 23:49:15 +0900

    Initial commit

---
+++ script.js
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Audrey Ng's Links</title>
+  <link rel="stylesheet" href="style.css" />
+</head>
+
+<body>
+  <div class="main">
+    <div class="up">
+      <div class="about">
+        <img class="pfp" src="assets/profile.jpg" alt="Profile picture of Audrey Ng" />
+        <h1 id="name">Audrey Ng</h1>
+        <p>Tour Contact 🌸 Book directly here!</p>
+      </div>
+      <div class="socials">
+        <a href="https://line.me/ti/p/yourLineID" target="_blank">
+          <img class="social-icons" src="assets/socials/line.png" alt="Line" />
+        </a>
+        <a href="https://wa.me/yourWhatsAppNumber" target="_blank">
+          <img class="social-icons" src="assets/socials/whatsapp.png" alt="WhatsApp" />
+        </a>
+        <a href="weixin://dl/chat?yourWeChatID" target="_blank">
+          <img class="social-icons" src="assets/socials/wechat.png" alt="WeChat" />
+        </a>
+        <a href="tel:+yourPhoneNumber" target="_blank">
+          <img class="social-icons" src="assets/socials/phone.png" alt="Phone" />
+        </a>
+      </div>
+    </div>
+
+    <h2>Book Your Tour</h2>
+    <div class="links">
+      <a href="https://exampletour.com/book?ref=audrey" target="_blank" class="btn">
+        <button type="button">Book Now 🌟</button>
+      </a>
+    </div>
+  </div>
+  <script src="script.js"></script>
+</body>
+
+</html>
